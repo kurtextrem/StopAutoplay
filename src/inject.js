a5c3cfbc514c075edd9e3296816a851a5142e6e7
Jacob Groß (kurtextrem) <kurtextrem@gmail.com>
2014-12-25 08:56:50 +0100

    Add mutation observer and better channel handling

--- src/inject.js
+++ src/inject.js
@@ -1,7 +1,8 @@
 +function (window) {
 	'use strict';
 
-	var document = window.document, location = window.location
+	var document = window.document,
+		location = window.location
 
 	var StopAutoplay = function () {
 		this.player = null
@@ -12,7 +13,7 @@
 		this.bind()
 	}
 
-	//StopAutoplay.VERSION = 1.5
+	//StopAutoplay.VERSION = '1.8'
 
 	StopAutoplay.prototype.init = function () {
 		if (!this.isWatchPage()) return
@@ -33,7 +34,7 @@
 		this.pause()
 		if (this.count < 10) {
 			window.setTimeout(function () {
-				if (document.hasFocus()) return
+				if (document.hasFocus()) return;
 				++this.count
 				this.stop()
 			}.bind(this), 4)
@@ -65,18 +66,12 @@
 	}
 
 	StopAutoplay.prototype.bind = function () {
-		window.addEventListener('focus', this.handleVisibilityChange.bind(this), false) // automatic playback
-		window.addEventListener('popstate', this.init.bind(this), false) // user back / for navigation
-		document.documentElement.addEventListener('transitionend', function (event) { // after ajax navigation, only works while tab is active: https://stackoverflow.com/questions/18397962/chrome-extension-is-not-loading-on-browser-navigation-at-youtube/18398921#18398921
-			if (event.propertyName === 'width' && event.target.id === 'progress') {
-				this.init()
-			}
-		}.bind(this), false)
-		// handle blur / focus + title change => transition in background?
+		window.addEventListener('focus', this.handleVisibilityChange.bind(this), false) // extended version: automatic playback
+		new MutationObserver(this.init.bind(this)).observe(document.body, { attributes: true })
 	}
 
 	StopAutoplay.prototype.isWatchPage = function () {
-		return (location.pathname === '/watch' || location.pathname.indexOf('/user/') !== -1) && location.search.indexOf('list=') === -1 // disabling / enabling is in the playlist player
+		return (location.pathname === '/watch' && location.search.indexOf('list=') === -1) || (location.pathname.indexOf('/channel/') !== -1 || location.pathname.indexOf('/user/') !== -1) // Playlist or channel page
 	}
 
 	// start
