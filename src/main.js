dbfe8fe3b4c3ac2e5652921e9f0a5331fa101a4f
Jacob Groß (kurtextrem) <kurtextrem@gmail.com>
2016-01-17 10:08:23 +0100

    Merge changes from master

--- src/main.js
+++ src/main.js
@@ -20,20 +20,23 @@
 	 * Installs an observer which waits for video elements.
 	 *
 	 * @author 	Jacob Groß
-	 * @date   	2015-08-25
+	 * @date   	2016-01-17
 	 */
 	StopAutoplay.prototype.waitForPlayer = function () {
+		var that = this
 		var observer = new MutationObserver(function (mutations) {
-			Object.keys(mutations).map(function (key) {
-				var mutation = mutations[key].addedNodes
-				for (var i = 0; i < mutation.length; i++) {
-					if (mutation[i].nodeName !== 'VIDEO') continue
-					console.log('mutation', mutation[i])
+			for (var i = 0; i < mutations.length; i++) {
+				var mutation = mutations[i].addedNodes
+				for (var x = 0; x < mutation.length; x++) {
+					if (mutation[x].nodeName !== 'VIDEO' && mutation[x].nodeName !== 'OBJECT') continue
+
+					console.log('mutation', mutation[x])
+
 					observer.disconnect() // waiting is over
-					return this.bindPlayer(mutation[i])
+					return that.bindPlayer(mutation[x])
 				}
-			}.bind(this))
-		}.bind(this))
+			}
+		})
 		observer.observe(document, { childList: true, subtree: true })
 	}
 
@@ -47,6 +50,7 @@
 	StopAutoplay.prototype.bindPlayer = function (player) {
 		console.log('binding', player)
 
+		// while buffering
 		if (player.readyState > 1) {
 			this.stopAutoplay(player)
 		}
@@ -85,9 +89,7 @@
 	 */
 	StopAutoplay.prototype.stopAutoplay = function (player) {
 		console.log('stopAutoplay')
-		if (!document.hasFocus()) {
-			this._pause(player)
-		}
+		this._pause(player)
 	}
 
 	/**
@@ -105,9 +107,6 @@
 
 	// start
 	new StopAutoplay()
+
+	console.log('loaded')
 }(window);
-
-console.log('loaded')
-
-// speedier .bind
-+function(n,t){"use strict";var e=n.bind;t.defineProperty(n,"bind",{value:function(n){var t=this;return 1===arguments.length?function(){return t.apply(n,arguments)}:e.apply(t,arguments)}})}(Function.prototype,Object);
