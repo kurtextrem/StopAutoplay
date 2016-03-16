aa318b0dbdadb86a744fa5fa8bdd07cadf997ecb
Jacob Groß <kurtextrem@gmail.com>
2016-03-16 15:55:48 +0100

    Feature: Mute + play&mute ads
    in the background

--- src/main.js
+++ src/main.js
@@ -1,67 +1,61 @@
-+function (window) {
+!function (window) {
 	'use strict'
 
-	var document = window.document
+	var document = window.document,
+		extended = true
+
+	// StopAutoplay.VERSION = '4.00'
 
 	/**
-	 * The constructor, binds and initializes vars.
+	 * Issues the pause command on the player element.
 	 *
 	 * @author 	Jacob Groß
 	 * @date   	2015-07-07
 	 */
-	var StopAutoplay = function () {
-		this.waitForPlayer()
-		this.bindGeneral()
-	}
-
-	// StopAutoplay.VERSION = '3.01'
-
-	/**
-	 * Installs an observer which waits for video elements.
-	 *
-	 * @author 	Jacob Groß
-	 * @date   	2016-01-17
-	 */
-	StopAutoplay.prototype.waitForPlayer = function () {
-		var that = this
-		var observer = new MutationObserver(function (mutations) {
-			for (var i = 0; i < mutations.length; i++) {
-				var mutation = mutations[i].addedNodes
-				for (var x = 0; x < mutation.length; x++) {
-					if (mutation[x].nodeName !== 'VIDEO' && mutation[x].nodeName !== 'OBJECT') continue
-
-					console.log('mutation', mutation[x])
-
-					observer.disconnect() // waiting is over
-					return that.bindPlayer(mutation[x])
-				}
-			}
-		})
-		observer.observe(document, { childList: true, subtree: true })
+	function _pause(player) {
+		console.log('pause', player)
+		if (player.pause)
+			return player.pause()
+		player.pauseVideo()
 	}
 
 	/**
-	 * Binds player specific events.
+	 * Stops the player, when the tab has focus.
 	 *
 	 * @author 	Jacob Groß
-	 * @date   	2015-08-25
-	 * @param  	{Object}   	player
+	 * @date   	2015-07-29
 	 */
-	StopAutoplay.prototype.bindPlayer = function (player) {
-		console.log('binding', player)
-
-		// while buffering
-		if (player.readyState > 1) {
-			this.stopAutoplay(player)
+	function stopAutoplay(player) {
+		console.log('stopAutoplay', player)
+		if (!document.hasFocus()) {
+			_pause(player)
 		}
+	}
 
-		console.log('add debug', this.addDebugListener.apply(this, [player]))
-		player.addEventListener('canplaythrough', this.stopAutoplay.bind(this, player))
-		// YouTube experiment which sets a timeout and afterwards plays the video
-		player.addEventListener('playing', this.stopAutoplay.bind(this, player))
+	/**
+	 * Issues the play command on the player element.
+	 *
+	 * @author 	Jacob Groß
+	 * @date   	2015-07-07
+	 */
+	function _play(player) {
+		console.log('play', player)
+		if (player.play)
+			return player.play()
+		player.playVideo()
+	}
 
-		/** Handler for the "Extended" version. */
-		window.addEventListener('focus', this.handleVisibilityChange.bind(this, player))
+	/**
+	 * Event handler when the tab gets visible.
+	 *
+	 * @author 	Jacob Groß
+	 * @date   	2015-07-29
+	 */
+	function handleVisibilityChange(player) {
+		window.setTimeout(function () {
+			if (!document.hidden)
+				_play(player)
+		}, 60)
 	}
 
 	/**
@@ -71,7 +65,7 @@
 	 * @date   	2016-01-17
 	 * @param  	{Object}   	player
 	 */
-	StopAutoplay.prototype.addDebugListener = function (player) {
+	function addDebugListener(player) {
 		player.addEventListener('canplay', function () {
 			console.log('canplay')
 		})
@@ -92,6 +86,76 @@
 		})
 	}
 
+	/**
+	 * Binds player specific events.
+	 *
+	 * @author 	Jacob Groß
+	 * @date   	2016-03-16
+	 * @param  	{Object}   	player
+	 */
+	function bindPlayer(player) {
+		console.log('binding', player)
+
+		// mute ad, play ad (real video player is inserted afterwards, so muting here doesn't affect the next video)
+		var parent = player.parentElement.parentElement
+		console.log('parent', parent)
+		if (parent.classList.contains('ad-created')) {
+			console.log('ad muted')
+			parent.getElementsByClassName('ytp-mute-button')[0].click()
+		}
+
+		// don't pause while buffering
+		if (player.readyState > 1) {
+			stopAutoplay(player)
+		}
+
+		console.log('add debug', addDebugListener.apply(null, [player]))
+
+		player.addEventListener('canplaythrough', stopAutoplay.bind(null, player))
+		// YouTube experiment which sets a timeout and afterwards plays the video
+		player.addEventListener('playing', function self() {
+			stopAutoplay(player)
+			player.removeEventListener('playing', self)
+		})
+
+		/** Handler for the "Extended" version. */
+		if (extended)
+			window.addEventListener('focus', handleVisibilityChange.bind(null, player))
+	}
+
+	/**
+	 * The constructor, binds and initializes vars.
+	 *
+	 * @author 	Jacob Groß
+	 * @date   	2015-07-07
+	 */
+	function StopAutoplay() {
+		this.waitForPlayer()
+		this.bindGeneral()
+	}
+
+	/**
+	 * Installs an observer which waits for video elements.
+	 *
+	 * @author 	Jacob Groß
+	 * @date   	2016-01-17
+	 */
+	StopAutoplay.prototype.waitForPlayer = function () {
+		var observer = new MutationObserver(function (mutations) {
+			for (var i = 0; i < mutations.length; i++) {
+				var mutation = mutations[i].addedNodes
+				for (var x = 0; x < mutation.length; x++) {
+					if (mutation[x].nodeName !== 'VIDEO' && mutation[x].nodeName !== 'OBJECT') continue
+					console.log('mutation', mutation[x])
+
+					observer.disconnect() // waiting is over
+					return bindPlayer(mutation[x])
+				}
+			}
+		})
+		observer.observe(document, { childList: true, subtree: true })
+	}
+
 
 	/**
 	 * Binds non /watch / channel specific event handlers.
@@ -107,64 +171,12 @@
 		window.onYouTubePlayerReady = function (player) {
 			console.log('player ready', player, player.getPlayerState(), player.getCurrentTime())
 
-			this.stopAutoplay(player)
+			stopAutoplay(player)
 
 			console.log(player.getCurrentTime())
 
 			if (original) original()
 		}.bind(this)
-	}
-
-	/**
-	 * Stops the player, when the tab has focus.
-	 *
-	 * @author 	Jacob Groß
-	 * @date   	2015-07-29
-	 */
-	StopAutoplay.prototype.stopAutoplay = function (player) {
-		console.log('stopAutoplay', player)
-		if (!document.hasFocus()) {
-			this._pause(player)
-		}
-	}
-
-	/**
-	 * Issues the pause command on the player element.
-	 *
-	 * @author 	Jacob Groß
-	 * @date   	2015-07-07
-	 */
-	StopAutoplay.prototype._pause = function (player) {
-		console.log('pause', player)
-		if (player.pause)
-			return player.pause()
-		player.pauseVideo()
-	}
-
-	/**
-	 * Issues the play command on the player element.
-	 *
-	 * @author 	Jacob Groß
-	 * @date   	2015-07-07
-	 */
-	StopAutoplay.prototype._play = function (player) {
-		console.log('play', player)
-		if (player.play)
-			return player.play()
-		player.playVideo()
-	}
-
-	/**
-	 * Event handler when the tab gets visible.
-	 *
-	 * @author 	Jacob Groß
-	 * @date   	2015-07-29
-	 */
-	StopAutoplay.prototype.handleVisibilityChange = function (player) {
-		window.setTimeout(function () {
-			if (!document.hidden)
-				this._play(player)
-		}.bind(this), 60)
 	}
 
 	// start
